59c672f15704a4f48f43ea67cc742bf175c3f01e
brunaalvescabral <brunaalvescabralb36@gmail.com>
2025-02-03 23:51:56 -0300

    Teste inicial: acionando os LEDs e botão

--- main.c
+++ main.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
+// Definição dos pinos
+#define LED_AZUL_PIN 11
+#define LED_VERMELHO_PIN 12
+#define LED_PINO_VERDE_PIN 13
+#define BOTAO_PIN 5
 
 int main()
 {
     stdio_init_all();
 
+    gpio_init(LED_AZUL_PIN);
+    gpio_init(LED_VERMELHO_PIN);
+    gpio_init(LED_PINO_VERDE_PIN);
+    gpio_init(BOTAO_PIN);
+
+    gpio_set_dir(LED_AZUL_PIN, GPIO_OUT);
+    gpio_set_dir(LED_VERMELHO_PIN, GPIO_OUT);
+    gpio_set_dir(LED_PINO_VERDE_PIN, GPIO_OUT);
+    gpio_set_dir(BOTAO_PIN, GPIO_IN);
+    gpio_pull_up(BOTAO_PIN);
+
     while (true) {
-        printf("Hello, world!\n");
+        if (gpio_get(BOTAO_PIN) == 0) {
+            printf("Botão foi pressionado!\n");
+            gpio_put(LED_AZUL_PIN, 1);
+            gpio_put(LED_VERMELHO_PIN, 1);  
+            gpio_put(LED_PINO_VERDE_PIN, 1);
+            sleep_ms(500);
+            gpio_put(LED_AZUL_PIN, 0);
+            gpio_put(LED_VERMELHO_PIN, 0);
+            gpio_put(LED_PINO_VERDE_PIN, 0);
+        }
+
         sleep_ms(1000);
     }
 }
